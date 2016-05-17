dd5df71e89cb2dbc7515b57f9f625dbae520ec8d
Bruno Vasconcelos <brunohvlemos@gmail.com>
2016-05-17 17:52:57 -0300

    vaaai

--- cart/views.py
+++ cart/views.py
@@ -9,14 +9,15 @@ from django.core.urlresolvers import reverse
 def show_cart(request, template_name="cart/_cart.html"):
 	if request.method == 'POST':
 		postdata = request.POST.copy()
-		if postdata['submit'] == 'Remover':
+		if postdata.get('submit') == 'Remover':
 			remove_from_cart(request)
-		if postdata['submit'] == 'Atualizar':
+		if postdata.get('submit') == 'Atualizar':
 			update_cart(request)
 	cart_items = get_cart_items(request)
 	page_title = 'Shopping Cart'
 	cart_subtotal = get_cart_subtotal(request)
 	return render_to_response(template_name, locals(), context_instance=RequestContext(request))
+
 
 def view_that_asks_for_money(request):
 	paypal_dict = {
@@ -41,3 +42,4 @@ def view_that_asks_for_money(request):
 	form = PayPalPaymentsForm(initial = paypal_dict)
 	context = {"form": form}
 	return render(request, "cart/payment.html", context)
+
